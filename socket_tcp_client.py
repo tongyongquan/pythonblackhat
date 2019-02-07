e376f34c7c8576b089cdac770ddc5b7db1334853
tongyongquan <1070969926@qq.com>
2019-02-07 21:08:01 +0800

    'tcp_server'

--- socket_tcp_client.py
+++ socket_tcp_client.py
@@ -1,20 +1,39 @@
 # encoding: utf-8
 
+'''
+    TCP是,面对连接,需要先建立连接,然后传输数据,最后再断开连接.
+    TCP的优点:
+        1.TCP是一种面向连接,需要先建立连接才可以传输数据.
+        2.可靠传输:
+               1).发送应答机制:TCP发送的每个报文段,都必须得到接收方的应答才认为这个TCP的报文段发
+                  送成功
+               2).错误效验:TCP会使用一种效验函数来检查传输的数据是否错误,传输和和接收的时候都会进
+                  行检查
+               3).超时重发:发送端发送一个报文段就会进入计时,如果再一定时间内没有收到应答就会重发这
+                  个报文段
+               4).阻塞管理:流量控制,用来防止因为对方发送消息过快,而导致消息接收不完全.
+'''
+
+# 1.导入socket模块
 import socket
 
-target_host = ''
-target_port = '0'
+# 2.使用socket模块,调用socket方法,创建套接字
+tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# 建立一个socket对象
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 3.建立和TCP服务器的连接
+# 使用connect()方法来建立和服务器的连接
+#                              windows IP    端口号
+tcp_client_socket.connect(("127.0.0.1", 8080))
 
-# 连接客户端
-client.connect((target_host, target_port))
+# 4.调用send方法,发送消息
+tcp_client_socket.send("你好,我叫宇宙无敌天下第一帅".encode())
 
-# 发送一些数据
-client.send('GET / HTTP/1.1\r\nHost: google.com\r\n\r\n')
+# 5.接收TCP服务器返回的消息
+# TCP使用recv()返回参数,返回的只有二进制的内容,没有IP地址和端口,这一点和UDP的recvfrom()不一样
+udp_data = tcp_client_socket.recv(1024)
+# 网络调试助手发送消息是默认gbk方式,所以咱们解包操作 gbk
+print("收到新的消息 : ", udp_data.decode("utf-8"))
 
-# 接受一些数据
-response = client.recv(4096)
+# 6.关闭套接字,释放电脑空间
+tcp_client_socket.close()
 
-print(response)
--- socket_tcp_server.py
+++ socket_tcp_server.py
@@ -1,29 +1,49 @@
 # _*_ coding:utf-8 _*_
 
-from socket import *
-import _thread
 
-def tcplink(skt,addr):
-    print(skt)
-    print(addr,"已经连接上...")
-    print('开始发送文件')
-    with open('./ww.jpg', 'rb') as f:
-        for data in f:
-            print(data)
-            skt.send(data)
-    f.close()
-    skt.close()
+'''
+    # 还是先讲讲代码思路
+    # 1.导入socket模块
+    # 2.创建套接字
+    # 3.绑定IP地址和端口
+    # 4.linsten监听,使套接字由主动变成被动
+    # 5.accept() 等待客户端的接入
+    # 6.recv() 接收客户端发来的消息
+    # 7.反馈消息给客户端,表示已接收到
+    # 8.关闭套接字
+    # 9.关闭服务器
+'''
+# 1.导入socket模块
+import socket
 
+# 2.创建套接字
+tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-HOST = "127.0.0.1"
-PORT = 23333
-ADDR = (HOST,PORT)
+# 3.使用bind()绑定IP地址和端口
+tcp_server_socket.bind(("127.0.0.1", 8080))
 
-server = socket(AF_INET,SOCK_STREAM)
-server.bind(ADDR)
-server.listen(5)
-
+# 4.linsten监听,使套接字由主动变成被动
+tcp_server_socket.listen(128)
 while True:
-    print("等待连接...")
-    skt,addr = server.accept()
+    # 5.accept() 等待客户端的接入
+    server_socket, ip_port = tcp_server_socket.accept()
+    print(ip_port, "客户端已经连接")
+    while True:
+        # 6.recv() 接收客户端发来的消息
+        server_data = server_socket.recv(1024)
+
+        # 对客户端发来的信息进行判断.如果有内容就解码打印显示,如果为空就退出
+        if server_data:
+            server_data = server_data.decode()
+            print("收到新消息 : ", ip_port, ":", server_data)
+            # 7.反馈消息给客户端,表示已接收到
+            server_socket.send("已收到".encode())
+        else:
+            # 如果发送内容为空则跳出 while True 循环
+            break
+
+    # 8.关闭套接字
+    server_socket.close()
+    # 9.关闭服务器,因为服务端一直在接收别人的信息,所以可以不用关闭.
+# tcp_client_socket.close()
 
